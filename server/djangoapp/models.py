2414a34f5ee1530142e496a43b5ed21dcde773d4
JsExplorer <x.js-@hotmail.com>
2022-03-27 14:19:58 +0000

    Update Carmake and CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name=models.CharField(null=False,max_length=30)
+    description=models.CharField(null=False,max_length=100)
 
+    def __str__(self):
+        return "Make: " + self.name + "," + "\n Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +24,29 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN='Sedan'
+    SUV='Suv'
+    WAGON='Wagon'
+    STYLE_CHOICES=[
+        (SEDAN,'Sedan'),
+        (WAGON,'Wagon'),
+        (SUV,'Suv')
+    ]
+    
+    make=models.ForeignKey(CarMake,null=False,on_delete=models.CASCADE)
+    dealer_id=models.IntegerField()
+    model_name=models.CharField(null=False,max_length=30)
+    style=models.CharField(
+        null=False,
+        max_length=20,
+        choices=STYLE_CHOICES,
+        default=SUV
+    )
+    year=models.DateField(default=now)
+
+    def __str__(self):
+        return "Make: " + str(self.make) + "," + "\n DealerId: " + str(self.dealer_id) + "," + "\n Model: " + self.model_name + "," + "\n Style: " + self.style + "," + "\n Year: " + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
