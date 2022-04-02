6bcc966b603e6a9692ff64a9217009365bc5eba8
JsExplorer <x.js-@hotmail.com>
2022-04-02 16:59:49 +0000

    update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -87,16 +87,20 @@ def registration_request(request):
             return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealerships(request):
+    if request.method == "GET":
+        context = {}
+        url = "https://2e451fd6-2a4c-4440-a3d1-6e60a8a2325a-bluemix.cloudantnosqldb.appdomain.cloud/api/dealership"
+        dealerships = get_dealers_from_cf(url)
+        context["dealership_list"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
+  
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
